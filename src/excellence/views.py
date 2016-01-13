e68866b64356d595e04428274268ca97595f033b
Alex Busaka <lxbusaka07@gmail.com>
2016-01-13 21:18:14 +0300

    fixed fading carousels

--- src/excellence/views.py
+++ src/excellence/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from high_schools.models import HighSchool
+from colleges.models import College
 
 
 class HomePage(TemplateView):
@@ -7,8 +8,9 @@ class HomePage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
         schools = HighSchool.objects.filter(featured='yes').order_by('?')[:4]
-
+        colleges = College.objects.all().order_by('?')
         context['schools'] = schools
+        context['colleges'] = colleges
         return context
 
 
