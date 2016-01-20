48a5d49289ab76da04279ef9d5abf918c24218ec
Alex Busaka <lxbusaka07@gmail.com>
2016-01-20 16:20:22 +0300

    fixed the local university details page layout.

--- src/universities/models.py
+++ src/universities/models.py
@@ -5,10 +5,35 @@ from django.db import models
 
 class LocalUniversities(models.Model):
     """Docstring for LocalUniversities. """
+    featured = models.CharField(max_length=500)
     university_name = models.CharField(max_length=200)
-    description = models.TextField()
-    university_photo = models.ImageField(upload_to='university_photos')
+    university_logo = models.ImageField(upload_to='universities/university_photos') 
+    university_motto = models.CharField(max_length=500)
+
+    university_photo1 = models.ImageField(upload_to='universities/university_photos') 
+    description1 = models.TextField(blank=True)
+    university_photo2 = models.ImageField(upload_to='universities/university_photos') 
+    description2 = models.TextField(blank=True)
+    university_photo3 = models.ImageField(upload_to='universities/university_photos') 
+    description3 = models.TextField(blank=True)
+    university_photo4 = models.ImageField(upload_to='universities/university_photos') 
+    description4 = models.TextField(blank=True)
+    university_photo5 = models.ImageField(upload_to='universities/university_photos') 
+    description5 = models.TextField(blank=True)
+
     published = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    po_box = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500)
+    email = models.EmailField()
+    website = models.CharField(max_length=500, blank=True)
+
+    course_one = models.CharField(max_length=500, blank=True)
+    course_two = models.CharField(max_length=500, blank=True)
+    course_three = models.CharField(max_length=500, blank=True)
+    course_four = models.CharField(max_length=500, blank=True)
+    course_five = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
         return self.university_name
@@ -16,10 +41,35 @@ class LocalUniversities(models.Model):
 
 class InternationalUniversities(models.Model):
     """Docstring for InternationalUniversities. """
+    featured = models.CharField(max_length=500)
     university_name = models.CharField(max_length=200)
-    description = models.TextField()
-    university_photo = models.ImageField(upload_to='university_photos')
+    university_logo = models.ImageField(upload_to='universities/university_photos') 
+    university_motto = models.CharField(max_length=500)
+
+    university_photo1 = models.ImageField(upload_to='universities/university_photos') 
+    description1 = models.TextField(blank=True)
+    university_photo2 = models.ImageField(upload_to='universities/university_photos') 
+    description2 = models.TextField(blank=True)
+    university_photo3 = models.ImageField(upload_to='universities/university_photos') 
+    description3 = models.TextField(blank=True)
+    university_photo4 = models.ImageField(upload_to='universities/university_photos') 
+    description4 = models.TextField(blank=True)
+    university_photo5 = models.ImageField(upload_to='universities/university_photos') 
+    description5 = models.TextField(blank=True)
+
     published = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    po_box = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500)
+    email = models.EmailField()
+    website = models.CharField(max_length=500, blank=True)
+
+    course_one = models.CharField(max_length=500, blank=True)
+    course_two = models.CharField(max_length=500, blank=True)
+    course_three = models.CharField(max_length=500, blank=True)
+    course_four = models.CharField(max_length=500, blank=True)
+    course_five = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
         return self.university_name
